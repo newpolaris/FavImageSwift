4f0cd9264dcea19ecd8385f6d9fcfc730a18df42
newpolaris <newpolaris@gmail.com>
2014-06-25 00:41:14 +0900

    Initial Commit

---
+++ HelloTwitter/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  HelloTwitter
+//
+//  Created by newpolaris on 6/25/14.
+//  Copyright (c) 2014 newpolaris. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
